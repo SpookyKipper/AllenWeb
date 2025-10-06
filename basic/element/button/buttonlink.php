466702f9f7c9bab7c28b49595be131cfd5d9565d
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2025-10-06 10:31:34 +0800

    新增 ButtonLink 類別的 Create 方法，以支持更多參數並改進連結生成邏輯

--- basic/element/button/buttonlink.php
+++ basic/element/button/buttonlink.php
@@ -4,9 +4,34 @@ namespace Allen\Basic\Element\Button;
 
 use Allen\Basic\Element\{A, Button};
 use Allen\Basic\Element\Enum\Target;
+use Allen\Basic\Util\{Uri};
 
 class ButtonLink extends Button
 {
+	public static function Create(
+		?string $content = null,
+		?string $link = null,
+		bool $lang = false,
+		bool $newTab = false,
+		bool $querySlash = false,
+		?string $id = null,
+		?array $class = null,
+		?array $style = null,
+	): self {
+		return new self(
+			content: $content,
+			id: $id,
+			class: $class,
+			style: $style,
+			href: Uri::Link(
+				url: $link,
+				lang: $lang,
+				querySlash: $querySlash,
+			),
+			target: $newTab ? Target::Blank
+				: null,
+		);
+	}
 	protected A $a;
 	public function __construct(
 		?array $attribute = null,
