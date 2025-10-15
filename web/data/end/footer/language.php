14187a976f89dda246dff854d7a17fc24d24c93b
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2025-10-16 00:47:32 +0800

    修正語言選擇下拉選單的 onchange 事件，新增 hash 支援以保持頁面狀態

--- web/data/end/footer/language.php
+++ web/data/end/footer/language.php
@@ -6,7 +6,7 @@ use Allen\Basic\Util\Uri;
 	<p>
 		<label>
 			<span class="material-symbols-outlined">language</span>
-			<select class="max-width" onchange="location = `${this.value}`;">
+			<select class="max-width" onchange="location = `${this.value}${window.location.hash}`;">
 				<?php foreach (Language::GetSupport() as $lang) { ?>
 					<option <?= $lang === Language::Get() ? 'selected ' : '' ?>value="<?= $lang === 'zh-Hant-TW' ? Uri::Parse($_SERVER['REQUEST_URI'])->RemoveQuery('lang')->Get() : Uri::Parse($_SERVER['REQUEST_URI'])->AddQuery('lang', $lang)->Get() ?>"><?= Language::GetName($lang) ?></option>
 				<?php } ?>
