6fd86daa29be09d83a8368300a7493876b787785
AS_Allen_Shih <65409535+ASAllenShih@users.noreply.github.com>
2025-10-22 13:12:36 +0800

    重構 Script 類別，將 ScriptType 更改為 EnumScriptType，並新增 Module 方法以支持模組化腳本

--- basic/element/script.php
+++ basic/element/script.php
@@ -2,6 +2,7 @@
 
 namespace Allen\Basic\Element;
 
+use Allen\Basic\Element\Enum\ScriptType as EnumScriptType;
 use Allen\Basic\Element\Trait\{ScriptType, Src};
 
 class Script extends Element
@@ -9,7 +10,7 @@ class Script extends Element
 	use Src, ScriptType;
 	public function __construct(
 		?string $src = null,
-		?ScriptType $type = null,
+		?EnumScriptType $type = null,
 		?string $content = null,
 	) {
 		parent::__construct(
@@ -18,5 +19,16 @@ class Script extends Element
 		);
 		if (!is_null($src)) $this->SrcSet($src);
 		if (!is_null($type)) $this->TypeSet($type);
+	}
+	public static function Module(
+		?string $src = null,
+		?string $content = null,
+	): self {
+		$script = new self(
+			src: $src,
+			type: EnumScriptType::Module,
+			content: $content,
+		);
+		return $script;
 	}
 }
